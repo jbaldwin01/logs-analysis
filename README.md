ba166b486a1b0706fb20462f85fe98f160c99ec6
Jeffrey Baldwin <jbaldwin01@snet.net>
2019-09-25 18:49:23 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# logs-analysis
-Full Stack Web Developer Nanodegree project
+# Full Stack Web Developer Nanodegree Project: Logs Analysis
+
+This project was completed as part of the Udacity Full Stack Web Developer Nanodegree.
+
+The task is to create a reporting tool that prints out reports (in plain text) based on the data in a PostgreSQL database. 
+This reporting tool is a Python program using the psycopg2 module to connect to the database.
+
+[Project rubric](https://review.udacity.com/#!/rubrics/277/view)
+
+## Program name:
+  project-logs.py
+
+## Project Dependencies
+
+1 log_requests view:
+```sql
+create view log_requests as
+SELECT date(log."time") AS date,
+    count(log."time") AS requests
+   FROM log
+  GROUP BY (date(log."time"))
+  ORDER BY (date(log."time"));
+```
+
+2 error_stats view:
+```sql
+create view log_requests as
+SELECT date(log."time") AS date,
+    count(log."time") AS requests
+   FROM log
+  GROUP BY (date(log."time"))
+  ORDER BY (date(log."time"));
+```
+
+## Running the program
+
+```python logs-analysis.py```
---
+++ logs-analysis.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+DBNAME = "news"
+
+dbconn = psycopg2.connect(database=DBNAME)
+c = dbconn.cursor()
+query1 = """select title, count(path) as views
+from articles
+join log on articles.slug = substring(log.path from 10)
+where status = '200 OK'
+and length(path) > 1
+group by title
+order by views desc
+limit 3;"""
+
+query2 = """select name, count(path) as views
+from authors
+join articles on authors.id = articles.author
+join log on articles.slug = substring(log.path from 10)
+where status = '200 OK'
+and length(path) > 1
+group by name
+order by views desc;"""
+
+query3 = """select date, round(cast(error_percentage as numeric), 1)
+as percent_errors
+from error_stats
+where error_percentage > 1;"""
+
+# Solution 1
+c.execute(query1)
+rows = c.fetchall()
+
+print("Question one solution:")
+for row in rows:
+    print("'" + row[0] + "'" + " - " + str(row[1]) + " views")
+
+# Solution 2
+c.execute(query2)
+rows = c.fetchall()
+
+print("\nQuestion two solution:")
+for row in rows:
+    print(row[0] + " - " + str(row[1]) + " views")
+
+# Solution 3
+c.execute(query3)
+rows = c.fetchall()
+
+print("\nQuestion three solution:")
+for row in rows:
+    print(str(row[0]) + " - " + str(row[1]) + "% errors")
+
+dbconn.close()
